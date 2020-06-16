fb917e3c032707a20fa1bfa553ce86765e90d6a5
andrea <andreatwelve@sina.cn>
2020-06-16 17:01:39 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+console.log(name.toUpperCase());
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+function capitalize(str) {
+  var s = str.split(/\s+/); //以空白为分界线
+  for (var i = 0; i < s.length; i++) {
+    s[i] = s[i].substring(0,1).toUpperCase() + s[i].substring(1)
+  }
+  return s.join(' ')
+}
+console.log(capitalize(sentence));
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+console.log(money.substr(1));
